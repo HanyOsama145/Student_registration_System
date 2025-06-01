d81b317391a636c988e90a3c04b62e270e8309f6
Hany Osama <70530646+HanyOsama145@users.noreply.github.com>
2025-06-01 14:09:09 +0300

    Delete src/main directory

--- src/main/java/com/example/project_phase_two/CourseException.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_phase_two;
-
-public class CourseException extends Exception {
-    public CourseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/example/project_phase_two/Registrable.java
+++
@@ -1,8 +0,0 @@
-package com.example.project_phase_two;
-
-import java.util.List;
-
-public interface Registrable {
-    void register(Course course);
-    void drop(Course course);
-}
